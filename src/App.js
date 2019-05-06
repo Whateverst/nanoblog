1f5ffdd04bdb752bf4e28ceb387516d8dec0e96b
Michał Bałos <michal@Michas-MBP.home>
2019-05-06 14:31:50 +0200

    Added topbar component

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ import "firebase/auth";
 import "firebase/firestore";
 import firebaseConfig from './firebaseConfig.js';
 
+//components
+import Topbar from './components/Topbar';
+
 class App extends Component {
 
   componentWillMount() {
@@ -18,21 +21,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo}  className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-            
-          >
-            Learn React
-          </a>
-        </header>
+       <Topbar></Topbar>
       </div>
     );
   }
