2bf5f1578654daf845e9d199ec77f6e8e7018256
Michał Bałos <michal@Michas-MBP.home>
2019-05-06 17:34:49 +0200

    Added firebase connection

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as firebase from "firebase/app";
+
+// Add the Firebase products that you want to use
+import "firebase/auth";
+import "firebase/firestore";
+import firebaseConfig from './firebaseConfig.js';
 
 class App extends Component {
+
+  componentWillMount() {
+    firebase.initializeApp(firebaseConfig);
+  }
+
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo}  className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
@@ -16,6 +28,7 @@ class App extends Component {
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
+            
           >
             Learn React
           </a>
