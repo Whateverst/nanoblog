bc74028feab318e1bce8153a8f2838ab7f852179
Michał Bałos <michal@Michas-MBP.home>
2019-05-06 14:39:56 +0200

    Changed appearance of navbar

--- src/components/Topbar.js
+++ src/components/Topbar.js
@@ -1,19 +1,26 @@
 import React from "react";
 
 //bootstrap
-import Navbar from 'react-bootstrap/Navbar';
-import NavbarBrand from 'react-bootstrap/NavbarBrand';
-import Nav from 'react-bootstrap/Nav';
-import FormControl from 'react-bootstrap/FormControl';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-import NavLink from 'react-bootstrap/NavLink';
+import Navbar from "react-bootstrap/Navbar";
+import NavbarBrand from "react-bootstrap/NavbarBrand";
+import Nav from "react-bootstrap/Nav";
+import NavLink from "react-bootstrap/NavLink";
 
 class Topbar extends React.Component {
-
   render() {
     return (
-      <Button variant="primary">Primary</Button>);
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <NavbarBrand href="#home">GastroBlog</NavbarBrand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto" />
+          <Nav>
+            <NavLink href="#signup">Zaloguj się</NavLink>
+            <NavLink href="#add">Dodaj post</NavLink>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    );
   }
 }
 
