6ec8a4cf65385fc5cb1133d308a06a4999a829cf
Michał Bałos <michal@Michas-MBP.home>
2019-05-06 14:31:50 +0200

    Added topbar component

---
+++ src/components/Topbar.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+//bootstrap
+import Navbar from 'react-bootstrap/Navbar';
+import NavbarBrand from 'react-bootstrap/NavbarBrand';
+import Nav from 'react-bootstrap/Nav';
+import FormControl from 'react-bootstrap/FormControl';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import NavLink from 'react-bootstrap/NavLink';
+
+class Topbar extends React.Component {
+
+  render() {
+    return (
+      <Button variant="primary">Primary</Button>);
+  }
+}
+
+export default Topbar;
