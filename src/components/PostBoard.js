a3fdb1d489156fa2836fd8bbe22d4189a9c16d0e
Michal <miketopolsky1@interia.pl>
2019-05-08 12:26:14 +0200

    small fix to postboard

--- src/components/PostBoard.js
+++ src/components/PostBoard.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 
 class PostBoard extends Component {
     render() {
-        <h1>board</h1>
+        return (
+            <h1>board</h1>
+        )
     }
 }
 
