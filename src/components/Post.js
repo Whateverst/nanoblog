4bd3b4842a23dfe0071ee223e03c2cc3d5d70a49
afternun <michal.kacper.balos@gmail.com>
2019-05-31 23:34:48 +0200

    Added comments and votes

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,61 +1,152 @@
-import React, { Component } from 'react';
-import Card from 'react-bootstrap/Card';
-import ListGroup from 'react-bootstrap/ListGroup';
+import React, { Component } from "react";
+import Card from "react-bootstrap/Card";
+import ListGroup from "react-bootstrap/ListGroup";
+import { Form, Button } from "react-bootstrap";
+import * as firebase from "firebase/app";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faPlus);
 
 class Post extends Component {
-    render() {
-        let ingredients;
-        if (this.props.post.ingredients.length !== 0) {
-            ingredients = this.props.post.ingredients.map(function(ingredient, index) {
-                return (
-                    <ListGroup.Item key={index}>
-                        Nazwa: {ingredient.name}, Kalorie: {ingredient.calories}, Ilość: {ingredient.amount}
-                    </ListGroup.Item>
-                )
-            });
-        } else {
-            ingredients = <div></div>
-        }
-        
-        let comments;
-        if (this.props.post.comments.length !== 0) {
-            comments = this.props.post.comments.map(function(comment, index) {
-                return (
-                    <ListGroup.Item key={index}>
-                        <span className="font-weight-bold">{comment.username}</span>: {comment.text}
-                    </ListGroup.Item>
-                )
-            })
-        } else {
-            comments = <div></div>
-        }
-        
+  constructor(props) {
+    super(props);
+    this.state = {
+      comment: ""
+    };
+  }
+
+  handleChange = name => event => {
+    this.setState({ [name]: event.target.value });
+    console.log(event.target.value);
+  };
+
+  addComent = (postId, item) => {
+    if (item.text) {
+      let db = firebase.firestore();
+      let posts = db.collection("posts");
+      const post = posts.doc(postId);
+
+      post
+        .update({
+          comments: firebase.firestore.FieldValue.arrayUnion({
+            text: item.text,
+            username: item.username
+          })
+        })
+        .then(this.props.getPosts());
+    } else alert("Comment cannot be empty");
+  };
+
+  upVotePost = (postId, currentVotes) => {
+    let db = firebase.firestore();
+    let posts = db.collection("posts");
+    const post = posts.doc(postId);
+    post
+      .update({
+        votes: currentVotes
+      })
+      .then(this.props.getPosts());
+  };
+
+  render() {
+    let ingredients;
+    if (this.props.post.ingredients.length !== 0) {
+      ingredients = this.props.post.ingredients.map(function(
+        ingredient,
+        index
+      ) {
         return (
-            <Card bg="secondary">
-                <Card.Body>
-                    <Card bg="light">
-                        <Card.Header>ID: {this.props.post.id}</Card.Header>
-                        <Card.Body>
-                            <Card.Title>{this.props.post.title}</Card.Title>
-                            <Card.Subtitle>Wpis użytkownika: {this.props.post.username}</Card.Subtitle>
-                            <Card.Text>{this.props.post.text}</Card.Text>
-                            <Card.Subtitle>Składniki</Card.Subtitle>
-                        </Card.Body>
-                        <ListGroup className="list-group-flush">{ingredients}</ListGroup>
-                    </Card>
-                    <Card>
-                        <Card.Header>Komentarze</Card.Header>
-                        <ListGroup className="list-group-flush">{comments}</ListGroup>
-                        <Card.Body>
-                            <Card.Link href="#">Dodaj komentarz</Card.Link>
-                            <Card.Link href="#">Link</Card.Link>
-                        </Card.Body>
-                        
-                    </Card>
-                </Card.Body>
-            </Card>
-        )
+          <ListGroup.Item key={index}>
+            Nazwa: {ingredient.name}, Kalorie: {ingredient.calories}, Ilość:{" "}
+            {ingredient.amount}
+          </ListGroup.Item>
+        );
+      });
+    } else {
+      ingredients = <div />;
     }
+
+    let comments;
+    if (this.props.post.comments.length !== 0) {
+      comments = this.props.post.comments.map(function(comment, index) {
+        return (
+          <ListGroup.Item key={index}>
+            <span className="font-weight-bold">{comment.username}</span>:{" "}
+            {comment.text}
+          </ListGroup.Item>
+        );
+      });
+    } else {
+      comments = <div />;
+    }
+
+    return (
+      <div style={{ borderBottom: "10px solid white" }}>
+        <Card.Body style={{ width: "100vw" }}>
+          <Card bg="light" style={{ width: "90vw" }}>
+            <Card.Header
+              style={{
+                display: "flex",
+                justifyContent: "flex-end",
+                alignItems: "center",
+                justifyContent: "space-between"
+              }}
+            >
+              <Card.Title>{this.props.post.title}</Card.Title>
+              <Button
+                style={{ maxWidth: "50px" }}
+                onClick={() =>
+                  this.upVotePost(this.props.post.id, this.props.post.votes + 1)
+                }
+              >
+                <FontAwesomeIcon icon={faPlus} style={{ marginRight: "2%" }} />
+                {this.props.post.votes}
+              </Button>
+            </Card.Header>
+            <Card.Body>
+              <Card.Subtitle />
+              <Card.Text>{this.props.post.text}</Card.Text>
+              <Card.Subtitle>Składniki</Card.Subtitle>
+            </Card.Body>
+            <ListGroup className="list-group-flush">{ingredients}</ListGroup>
+            <Card.Footer>
+              Added by: <strong>{this.props.post.username} </strong>
+            </Card.Footer>
+          </Card>
+          <Card style={{ width: "90vw" }}>
+            <Card.Header>Komentarze</Card.Header>
+            <ListGroup
+              className="list-group-flush"
+              style={{ maxHeight: "200px", overflow: "scroll" }}
+            >
+              {comments}
+            </ListGroup>
+            <Card.Body>
+              <Form.Group controlId="text">
+                <Form.Control
+                  type="text"
+                  placeholder="Add comment"
+                  onChange={this.handleChange("comment")}
+                />
+              </Form.Group>
+              <Button
+                onClick={() =>
+                  this.addComent(this.props.post.id, {
+                    text: this.state.comment,
+                    username: this.props.username
+                  })
+                }
+              >
+                Dodaj komentarz
+              </Button>
+            </Card.Body>
+          </Card>
+        </Card.Body>
+      </div>
+    );
+  }
 }
 
-export default Post
+export default Post;
--- src/components/PostBoard.js
+++ src/components/PostBoard.js
@@ -1,20 +1,52 @@
-import React, { Component } from 'react';
-import Post from './Post';
+import React, { Component } from "react";
+import Post from "./Post";
+import * as firebase from "firebase/app";
 
-class PostBoard extends Component {
-    render() {
-        const posts = this.props.posts;
-        
-        if (posts !== []) {
-            var items = posts.map(function(post, index) {
-                return <Post post={post} key={post.id}/>
-            })
-            return (
-                <div className="post-board">{items}</div>
-            );
-        }
-        
+class PostBoard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: []
+    };
+    this.getPosts = this.getPosts.bind(this);
+  }
+
+  componentWillMount() {
+    this.getPosts();
+  }
+
+  async getPosts() {
+    const snapshot = await firebase
+      .firestore()
+      .collection("posts")
+      .get();
+    let data = snapshot.docs.map(function(doc) {
+      let post = doc.data();
+      post.id = doc.id;
+      return post;
+    });
+    data.sort((a, b) => b.votes - a.votes);
+    this.setState({ posts: data });
+  }
+
+  render() {
+    const posts = this.state.posts;
+    const username = this.props.username;
+    const getPosts = this.getPosts;
+    if (posts !== []) {
+      var items = posts.map(function(post) {
+        return (
+          <Post
+            post={post}
+            key={post.id}
+            username={username}
+            getPosts={getPosts}
+          />
+        );
+      });
+      return <div className="post-board">{items}</div>;
     }
+  }
 }
 
-export default PostBoard
+export default PostBoard;
