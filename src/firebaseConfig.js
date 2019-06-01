a7c183b5a8eeba8bcdfd37017dca1773ea63d7fd
Whateverst <czuchnowski.t@gmail.com>
2019-06-01 16:22:24 +0200

    add_firebaseConfig

---
+++ src/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDQSWHXv_F6i8LUmLwkKUQmtsvMG5vgVQc",
+    authDomain: "nanoblog-706ff.firebaseapp.com",
+    databaseURL: "https://nanoblog-706ff.firebaseio.com",
+    projectId: "nanoblog-706ff",
+    storageBucket: "nanoblog-706ff.appspot.com",
+    messagingSenderId: "378562242644",
+    appId: "1:378562242644:web:3fb43aca82317485"
+};
+
+export default firebaseConfig;
