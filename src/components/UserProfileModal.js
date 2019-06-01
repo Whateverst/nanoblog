db5a5ddce7c1e042b56470a332d64d276980982e
afternun <michal.kacper.balos@gmail.com>
2019-06-01 13:06:08 +0200

    Added user posts

--- src/components/UserProfileModal.js
+++ src/components/UserProfileModal.js
@@ -9,6 +9,9 @@ import "firebase/firestore";
 import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import Card from "react-bootstrap/Card";
+import Accordion from "react-bootstrap/Accordion";
+import ListGroup from "react-bootstrap/ListGroup";
 
 class UserLoginRegisterModal extends React.Component {
   constructor(...args) {
@@ -21,21 +24,46 @@ class UserLoginRegisterModal extends React.Component {
   };
 
   saveProfileData = username => {
-    var user = firebase.auth().currentUser;
+    if (username) {
+      var user = firebase.auth().currentUser;
 
-    user
-      .updateProfile({
-        displayName: username
-      })
-      .then(function() {
-        alert("Po ponownym zalogowaniu nick zostanie zmieniony");
-      })
-      .catch(function(error) {
-        // An error happened.
-      });
+      user
+        .updateProfile({
+          displayName: username
+        })
+        .then(function() {
+          alert("Po ponownym zalogowaniu nick zostanie zmieniony");
+        })
+        .catch(function(error) {
+          // An error happened.
+        });
+    } else return;
   };
 
   render() {
+    let posts = this.props.posts;
+    console.log(posts);
+    let postsList = posts
+      .filter(post => post.username === this.props.username)
+      .map((post, index) => (
+        <Card>
+          <Accordion.Toggle as={Button} variant="link" eventKey={post.id}>
+            <strong>{post.title}</strong>
+          </Accordion.Toggle>
+          <Accordion.Collapse eventKey={post.id}>
+            <Card.Body>
+              {post.text}
+              {post.ingredients.map(ingredient => (
+                <ListGroup.Item>
+                  {ingredient.amount} {ingredient.name}, Calories:{" "}
+                  {ingredient.calories}
+                </ListGroup.Item>
+              ))}
+            </Card.Body>
+          </Accordion.Collapse>
+        </Card>
+      ));
+
     return (
       <Modal
         {...this.props}
@@ -44,18 +72,19 @@ class UserLoginRegisterModal extends React.Component {
         centered
       >
         <Modal.Header closeButton>
-          <Modal.Title id="contained-modal-title-vcenter">Profil</Modal.Title>
+          <Modal.Title id="contained-modal-title-vcenter">Profile</Modal.Title>
         </Modal.Header>
         <Modal.Body>
           <Form>
             <Form.Group controlId="formBasicEmail">
-              <Form.Label>Ustaw swój nick</Form.Label>
+              <Form.Label>Change your username</Form.Label>
               <Form.Control
                 type="text"
-                placeholder="Nick"
+                placeholder="Enter new username"
                 onChange={this.handleChange("nick")}
               />
             </Form.Group>
+            <Accordion>{postsList}</Accordion>
 
             <Button
               variant="primary"
@@ -65,7 +94,7 @@ class UserLoginRegisterModal extends React.Component {
               }}
               style={{ marginRight: "5px" }}
             >
-              Zapisz
+              Save
             </Button>
           </Form>
         </Modal.Body>
